149810f61ee6fc63981fb3092e277a0c1ef74bb8
Anita Koźlak <anita.kozlak.external@atos.net>
2022-08-29 15:43:49 +0200

    change add to ad

--- src/views/AdView.vue
+++ src/views/AdView.vue
@@ -12,8 +12,8 @@
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
 
-import BaseForm from "@/features/add/creation/BaseForm.vue";
-import MainSection from "@/features/add/creation/MainSection.vue";
+import BaseForm from "@/features/ad/creation/BaseForm.vue";
+import MainSection from "@/features/ad/creation/MainSection.vue";
 
 import Header from "@/components/Header.vue"; // @ is an alias to /src
 import Footer from "@/components/Footer.vue"; // @ is an alias to /src
@@ -26,7 +26,5 @@ import Footer from "@/components/Footer.vue"; // @ is an alias to /src
     Footer,
   },
 })
-export default class AddView extends Vue {}
+export default class AdView extends Vue {}
 </script>
-
-
