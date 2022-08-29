8a8b8a661cb66311364dfe6d49f3bc6dd3dd0ba3
Anita Koźlak <anita.kozlak.external@atos.net>
2022-08-29 13:51:45 +0200

    add BaseForm component

---
+++ src/features/add/creation/BaseForm.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="section-form mt-2">
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({
+  components: {},
+})
+export default class Header extends Vue {}
+</script>
+
+<style lang="scss">
+@import "@/assets/_variables.scss";
+.section-form {
+  background-color: $light-gray;
+  border-radius: 1rem;
+}
+</style>
