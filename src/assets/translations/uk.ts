a80d6061407c1ad6f95cd1e7901cdad79a1511e2
Dawid Perdek <perdekdawid@gmail.com>
2022-09-16 17:25:00 +0200

    BEDOK-26 - divide translations into multiple files per language

---
+++ src/assets/translations/uk.ts
@@ -0,0 +1,242 @@
+export default {
+    header: {
+        logoSubtitle: '[UK] i masz spanie!',
+            callToAction: '[UK] Wynajmij i zarabiaj bez opłat',
+            download: {
+            firstLine: {
+                beforeLogo: '[UK] Pobierz naszą aplikację',
+                    afterLogo: '[UK] na'
+            },
+            secondLine: '[UK] system Android i IOS'
+        },
+        login: '[UK] Zaloguj się'
+    },
+    footer: {
+        callToAction: '[UK] Zostań wynajmującym bez opłat',
+            informationSection: '[UK] Informacje',
+            prices: '[UK] Ceny',
+            cityList: '[UK] Lista miast',
+            partnerProgram: '[UK] Program partnerski',
+            faq: '[UK] Pytania i odpowiedzi Q&A',
+            help: '[UK] Centrum pomocy',
+            contact: '[UK] Kontakt',
+            about: '[UK] O bed!OK',
+            careers: '[UK] Kariera',
+            information: '[UK] Informacje',
+            termsOfCooperation: '[UK] Zasady współpracy',
+            privacyPolicy: '[UK] Oświadczenie o ochronie prywatności i plikach cookies',
+            cookies: '[UK] Zarządzaj ustawieniami dotyczącymi plików cookies',
+            clientSection: '[UK] klient',
+            login: '[UK] Logowanie',
+            registration: '[UK] Rejestracja',
+            specialOffers: '[UK] Oferty specjalne',
+            copyrightsNote: {
+            firstPart: '[UK] Prawa autorskie',
+                year: '[UK] 2022',
+                lastPart: '[UK] bed!Ok. Wszelkie prawa autorskie zastrzeżone.'
+        }
+    },
+    homeView: {
+        title: {
+            firstLine: '[UK] Потрібне житло?',
+                secondLine: '[UK] Wejdź na BedOK i masz spanie!',
+        },
+        looking: '[UK] szukam',
+            offering: '[UK] oferuję',
+            joinRoom: '[UK] NOWOŚĆ! Dołącz do pokoju',
+            citiesTitle: '[UK] W którym mieście chcesz znaleźć nocleg?',
+            howItWorks: '[UK] Jak to działa?',
+            subscribe: '[UK] Zapisz się aby otrzymywać najświeższe informacje i promocje',
+            freeAppLink: '[UK] Chcę otrzymać link do bezpłatnej aplikacji'
+    },
+    advertisementView: {
+        header: '[UK] Dodaj ogłoszenie',
+            mainSection: {
+            title: {
+                label: '[UK] Tytuł ogłoszenia*',
+                    tip: '[UK] od 7 do 70 znaków',
+                    placeholder: '[UK] Np. Pokój dwuosobowy w cichej okolicy',
+                    validationMessages: {
+                    length: '[UK] Tytuł musi mieć min. 7 znaków, max. 70 znaków.',
+                        required: '[UK] To pole jest wymagane.'
+                }
+            },
+            address: {
+                label: '[UK] Adres*',
+                    cityPlaceholder: '[UK] Miasto',
+                    zipCodePlaceholder: '[UK] Kod pocztowy',
+                    streetPlaceholder: '[UK] Ulica',
+                    streetNumberPlaceholder: '[UK] Nr domu',
+                    flatNumberPlaceholder: '[UK] Nr lokalu',
+                    validationMessages: {
+                    required: '[UK] To pole jest wymagane.'
+                }
+            },
+            photos: {
+                label: '[UK] Zdjęcia*',
+                    tip: '[UK] min. 1 szt. , max 8 szt.'
+            }
+        },
+        hostSection: {
+            hostName: {
+                label: '[UK] Nazwa gospodarza*',
+                    placeholder: '[UK] Imię',
+                    validationMessages: {
+                    required: '[UK] To pole jest wymagane.'
+                }
+            },
+            phoneNumber: {
+                label: '[UK] Nr telefonu*',
+                    placeholder: '[UK] Np. +48 000 000 000',
+                    validationMessages: {
+                    required: '[UK] To pole jest wymagane.'
+                }
+            },
+            email: {
+                label: '[UK] E-mail*',
+                    placeholder: "Np. jan_kowalski{'@'}o2.pl",
+                    validationMessages: {
+                    required: '[UK] To pole jest wymagane.',
+                        email: '[UK] Nieprawdiłowy email'
+                }
+            },
+            photo: {
+                label: '[UK] Zdjęcie gospodarza'
+            },
+            languages: {
+                label: '[UK] Języki, którymi posługuje się gospodarz*',
+                    validationMessages: {
+                    required: '[UK] To pole jest wymagane.'
+                }
+            },
+            communicators: {
+                label: '[UK] Komunikatory, których używa gospodarz*',
+                    tip: '[UK] Aplikacje do komnikacji przez internet',
+                    validationMessages: {
+                    required: '[UK] To pole jest wymagane.'
+                }
+            }
+        },
+        descriptionSection: {
+            description: {
+                label: '[UK] Opis*',
+                    tip: '[UK] maksimum 5000 znaków',
+                    tabs: {
+                    own: '[UK] Własny',
+                        preset1: '[UK] Szablon 1',
+                        preset2: '[UK] Szablon 2'
+                },
+                charactersCounter: '[UK] Pozostało {count} znaków'
+            },
+            roomSize: {
+                label: '[UK] Powierzchnia pokoju (m2)*',
+                    placeholder: '[UK] np. 4.86',
+                    validationMessages: {
+                    required: '[UK] To pole jest wymagane.'
+                }
+            },
+            splitIntoBeds: {
+                label: '[UK] Pokój dzielony na łóżka',
+                    value: '[UK] Podziel'
+            },
+            bedsCount: {
+                label: '[UK] Liczba łóżek w pokoju*',
+                    placeholder: '[UK] np. 6',
+                    validationMessages: {
+                    required: '[UK] To pole jest wymagane.'
+                }
+            },
+            roomType: {
+                label: '[UK] Typ pokoju'
+            },
+            freeBedsCount: {
+                label: '[UK] Liczba wolnych łóżek w pokoj*',
+                    validationMessages: {
+                    required: '[UK] To pole jest wymagane.'
+                }
+            }
+        },
+        currentGuestsSection: {
+            title: '[UK] Podaj informację o aktualnych lokatorach, tj. imię, wiek, języki, którymi się posługują',
+                name: {
+                placeholder: '[UK] Imię'
+            },
+            birthYear: {
+                label: '[UK] Rok urodzenia*'
+            }
+        },
+        paymentSection: {
+            paymentMethod: {
+                label: '[UK] Formy płatności:'
+            },
+            pricing: {
+                label: '[UK] Ceny (zł):'
+            },
+            day: '[UK] doba',
+                month: '[UK] miesiąc',
+                shortRentRange: '[UK] Ilość dni (od 1 do 30 dni)',
+                longRentRange: '[UK] Ilość miesięcy (powyżej 1 miesiąca)',
+                addPriceRange: '[UK] Dodaj kolejny zakres cen'
+        },
+        rulesSection: {
+            termsOfStay: '[UK] Zasady pobytu:',
+                animals: '[UK] Akceptujemy zwierzęta:',
+                curfew: '[UK] Cisza nocna (22:00 - 6:00):',
+                smoking: '[UK] Palenie w budynku dozwolone:',
+                others: {
+                label: '[UK] Inne:',
+                    placeholder: '[UK] np. brak parkingu, grill w ogrodzie'
+            }
+        },
+        equipmentSection: {
+            roomEquipment: {
+                label: '[UK] Wyposażenie pokoju',
+                    placeholder: '[UK] np. lampka, dywan'
+            },
+            sharedEquipment: {
+                label: '[UK] Wyposażenie strefy wspólnej',
+                    placeholder: '[UK] np. lampka, dywan'
+            }
+        },
+        actions: {
+            preview: {
+                label: '[UK] Zobacz podgląd ogłoszenia',
+                    tip: '[UK] (zobaczysz jak podgląd ogłoszenie przed jego publikacją)'
+            },
+            save: {
+                label: '[UK] Zapisz ogłoszenie',
+                    tip: '[UK] (nadal będziesz mieć możliwość wprowadzenia zmian w dowolnym momencie)'
+            },
+            publish: {
+                label: '[UK] Opublikuj ogłoszenie',
+                    tip: '[UK] (nadal będziesz mieć możliwość wprowadzenia zmian w dowolnym momencie)'
+            },
+            duplicate: {
+                label: '[UK] Zduplikuj ogłoszenie z wypełnionymi danymi',
+                    tip: '[UK] (zaoszczędzisz czas dodając pokoje w tym samymy budynku)'
+            }
+        }
+    },
+    searchInput: {
+        localization: {
+            label: '[UK] Lokalizacja',
+                placeholder: '[UK] Gdzie szukasz noclegu'
+        },
+        since: '[UK] Od kiedy',
+            to: '[UK] Do kiedy',
+            who: {
+            label: '[UK] Kto',
+                placeholder: '[UK] Ile osób'
+        },
+        search: '[UK] Szukaj'
+    },
+    roomCard: {
+        district: '[UK] Dzielnica:',
+            bedsInRoom: '[UK] {count} łóżka w pokoju',
+            dayPrice: '[UK] {price} / dzień',
+            bedDayPrice: '[UK] {price} / dzień / łóżko',
+            femaleRoom: '[UK] Pokój żenski',
+            tenants: '[UK] Lokatorzy:',
+            join: '[UK] Dołącz do pokoju'
+    },
+};
