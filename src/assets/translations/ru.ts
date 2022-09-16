43fdda84d5b674f2329749acf5fcafb8e2123238
Dawid Perdek <perdekdawid@gmail.com>
2022-09-16 17:25:00 +0200

    BEDOK-26 - divide translations into multiple files per language

---
+++ src/assets/translations/ru.ts
@@ -0,0 +1,242 @@
+export default {
+    header: {
+        logoSubtitle: '[RU] i masz spanie!',
+            callToAction: '[RU] Wynajmij i zarabiaj bez opłat',
+            download: {
+            firstLine: {
+                beforeLogo: '[RU] Pobierz naszą aplikację',
+                    afterLogo: '[RU] na'
+            },
+            secondLine: '[RU] system Android i IOS'
+        },
+        login: '[RU] Zaloguj się'
+    },
+    footer: {
+        callToAction: '[RU] Zostań wynajmującym bez opłat',
+            informationSection: '[RU] Informacje',
+            prices: '[RU] Ceny',
+            cityList: '[RU] Lista miast',
+            partnerProgram: '[RU] Program partnerski',
+            faq: '[RU] Pytania i odpowiedzi Q&A',
+            help: '[RU] Centrum pomocy',
+            contact: '[RU] Kontakt',
+            about: '[RU] O bed!OK',
+            careers: '[RU] Kariera',
+            information: '[RU] Informacje',
+            termsOfCooperation: '[RU] Zasady współpracy',
+            privacyPolicy: '[RU] Oświadczenie o ochronie prywatności i plikach cookies',
+            cookies: '[RU] Zarządzaj ustawieniami dotyczącymi plików cookies',
+            clientSection: '[RU] klient',
+            login: '[RU] Logowanie',
+            registration: '[RU] Rejestracja',
+            specialOffers: '[RU] Oferty specjalne',
+            copyrightsNote: {
+            firstPart: '[RU] Prawa autorskie',
+                year: '[RU] 2022',
+                lastPart: '[RU] bed!Ok. Wszelkie prawa autorskie zastrzeżone.'
+        }
+    },
+    homeView: {
+        title: {
+            firstLine: '[RU] Требуется проживание?',
+                secondLine: '[RU] Wejdź na BedOK i masz spanie!',
+        },
+        looking: '[RU] szukam',
+            offering: '[RU] oferuję',
+            joinRoom: '[RU] NOWOŚĆ! Dołącz do pokoju',
+            citiesTitle: '[RU] W którym mieście chcesz znaleźć nocleg?',
+            howItWorks: '[RU] Jak to działa?',
+            subscribe: '[RU] Zapisz się aby otrzymywać najświeższe informacje i promocje',
+            freeAppLink: '[RU] Chcę otrzymać link do bezpłatnej aplikacji'
+    },
+    advertisementView: {
+        header: '[RU] Dodaj ogłoszenie',
+            mainSection: {
+            title: {
+                label: '[RU] Tytuł ogłoszenia*',
+                    tip: '[RU] od 7 do 70 znaków',
+                    placeholder: '[RU] Np. Pokój dwuosobowy w cichej okolicy',
+                    validationMessages: {
+                    length: '[RU] Tytuł musi mieć min. 7 znaków, max. 70 znaków.',
+                        required: '[RU] To pole jest wymagane.'
+                }
+            },
+            address: {
+                label: '[RU] Adres*',
+                    cityPlaceholder: '[RU] Miasto',
+                    zipCodePlaceholder: '[RU] Kod pocztowy',
+                    streetPlaceholder: '[RU] Ulica',
+                    streetNumberPlaceholder: '[RU] Nr domu',
+                    flatNumberPlaceholder: '[RU] Nr lokalu',
+                    validationMessages: {
+                    required: '[RU] To pole jest wymagane.'
+                }
+            },
+            photos: {
+                label: '[RU] Zdjęcia*',
+                    tip: '[RU] min. 1 szt. , max 8 szt.'
+            }
+        },
+        hostSection: {
+            hostName: {
+                label: '[RU] Nazwa gospodarza*',
+                    placeholder: '[RU] Imię',
+                    validationMessages: {
+                    required: '[RU] To pole jest wymagane.'
+                }
+            },
+            phoneNumber: {
+                label: '[RU] Nr telefonu*',
+                    placeholder: '[RU] Np. +48 000 000 000',
+                    validationMessages: {
+                    required: '[RU] To pole jest wymagane.'
+                }
+            },
+            email: {
+                label: '[RU] E-mail*',
+                    placeholder: "Np. jan_kowalski{'@'}o2.pl",
+                    validationMessages: {
+                    required: '[RU] To pole jest wymagane.',
+                        email: '[RU] Nieprawdiłowy email'
+                }
+            },
+            photo: {
+                label: '[RU] Zdjęcie gospodarza'
+            },
+            languages: {
+                label: '[RU] Języki, którymi posługuje się gospodarz*',
+                    validationMessages: {
+                    required: '[RU] To pole jest wymagane.'
+                }
+            },
+            communicators: {
+                label: '[RU] Komunikatory, których używa gospodarz*',
+                    tip: '[RU] Aplikacje do komnikacji przez internet',
+                    validationMessages: {
+                    required: '[RU] To pole jest wymagane.'
+                }
+            }
+        },
+        descriptionSection: {
+            description: {
+                label: '[RU] Opis*',
+                    tip: '[RU] maksimum 5000 znaków',
+                    tabs: {
+                    own: '[RU] Własny',
+                        preset1: '[RU] Szablon 1',
+                        preset2: '[RU] Szablon 2'
+                },
+                charactersCounter: '[RU] Pozostało {count} znaków'
+            },
+            roomSize: {
+                label: '[RU] Powierzchnia pokoju (m2)*',
+                    placeholder: '[RU] np. 4.86',
+                    validationMessages: {
+                    required: '[RU] To pole jest wymagane.'
+                }
+            },
+            splitIntoBeds: {
+                label: '[RU] Pokój dzielony na łóżka',
+                    value: '[RU] Podziel'
+            },
+            bedsCount: {
+                label: '[RU] Liczba łóżek w pokoju*',
+                    placeholder: '[RU] np. 6',
+                    validationMessages: {
+                    required: '[RU] To pole jest wymagane.'
+                }
+            },
+            roomType: {
+                label: '[RU] Typ pokoju'
+            },
+            freeBedsCount: {
+                label: '[RU] Liczba wolnych łóżek w pokoj*',
+                    validationMessages: {
+                    required: '[RU] To pole jest wymagane.'
+                }
+            }
+        },
+        currentGuestsSection: {
+            title: '[RU] Podaj informację o aktualnych lokatorach, tj. imię, wiek, języki, którymi się posługują',
+                name: {
+                placeholder: '[RU] Imię'
+            },
+            birthYear: {
+                label: '[RU] Rok urodzenia*'
+            }
+        },
+        paymentSection: {
+            paymentMethod: {
+                label: '[RU] Formy płatności:'
+            },
+            pricing: {
+                label: '[RU] Ceny (zł):'
+            },
+            day: '[RU] doba',
+                month: '[RU] miesiąc',
+                shortRentRange: '[RU] Ilość dni (od 1 do 30 dni)',
+                longRentRange: '[RU] Ilość miesięcy (powyżej 1 miesiąca)',
+                addPriceRange: '[RU] Dodaj kolejny zakres cen'
+        },
+        rulesSection: {
+            termsOfStay: '[RU] Zasady pobytu:',
+                animals: '[RU] Akceptujemy zwierzęta:',
+                curfew: '[RU] Cisza nocna (22:00 - 6:00):',
+                smoking: '[RU] Palenie w budynku dozwolone:',
+                others: {
+                label: '[RU] Inne:',
+                    placeholder: '[RU] np. brak parkingu, grill w ogrodzie'
+            }
+        },
+        equipmentSection: {
+            roomEquipment: {
+                label: '[RU] Wyposażenie pokoju',
+                    placeholder: '[RU] np. lampka, dywan'
+            },
+            sharedEquipment: {
+                label: '[RU] Wyposażenie strefy wspólnej',
+                    placeholder: '[RU] np. lampka, dywan'
+            }
+        },
+        actions: {
+            preview: {
+                label: '[RU] Zobacz podgląd ogłoszenia',
+                    tip: '[RU] (zobaczysz jak podgląd ogłoszenie przed jego publikacją)'
+            },
+            save: {
+                label: '[RU] Zapisz ogłoszenie',
+                    tip: '[RU] (nadal będziesz mieć możliwość wprowadzenia zmian w dowolnym momencie)'
+            },
+            publish: {
+                label: '[RU] Opublikuj ogłoszenie',
+                    tip: '[RU] (nadal będziesz mieć możliwość wprowadzenia zmian w dowolnym momencie)'
+            },
+            duplicate: {
+                label: '[RU] Zduplikuj ogłoszenie z wypełnionymi danymi',
+                    tip: '[RU] (zaoszczędzisz czas dodając pokoje w tym samymy budynku)'
+            }
+        }
+    },
+    searchInput: {
+        localization: {
+            label: '[RU] Lokalizacja',
+                placeholder: '[RU] Gdzie szukasz noclegu'
+        },
+        since: '[RU] Od kiedy',
+            to: '[RU] Do kiedy',
+            who: {
+            label: '[RU] Kto',
+                placeholder: '[RU] Ile osób'
+        },
+        search: '[RU] Szukaj'
+    },
+    roomCard: {
+        district: '[RU] Dzielnica:',
+            bedsInRoom: '[RU] {count} łóżka w pokoju',
+            dayPrice: '[RU] {price} / dzień',
+            bedDayPrice: '[RU] {price} / dzień / łóżko',
+            femaleRoom: '[RU] Pokój żenski',
+            tenants: '[RU] Lokatorzy:',
+            join: '[RU] Dołącz do pokoju'
+    },
+};
