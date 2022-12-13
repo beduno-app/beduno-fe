c5180fc5a558b84552687eaac6c557c9fa840835
Anita Koźlak <anita.kozlak.external@atos.net>
2022-12-13 14:35:35 +0100

    devide advertisements list into active and inactive

--- src/features/ad/list/SingleHostAd.vue
+++ src/features/ad/list/SingleHostAd.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="row m-auto advertisement-section py-5" style="max-width: 1300px">
-    <div class="col-lg-3 col-md-4 col-xl-2 mb-3">
+    <div class="col-lg-3 col-xl-2 mb-3">
       <img
         src="../../../assets/img/placeholder.png"
         alt="host-image"
         class="host-image"
       />
     </div>
-    <div class="col-lg-5 col-md-8 pb-5">
+    <div class="col-lg-9 col-xl-4 pb-5">
       <div class="d-flex mb-3 align-items-center">
         <font-awesome-icon
           icon="fa-solid fa-location-dot"
@@ -29,7 +29,7 @@
         <div class="col-sm-8 col-lg-9 col text-green">1 nowa wiadomość</div>
       </div>
     </div>
-    <div class="col-lg-4 col-md-12">
+    <div class="col-lg-6 col-xl-3">
       <div class="d-flex align-items-end">
         <div class="col-sm-1 col-lg-2">
           <img
@@ -46,13 +46,60 @@
         <div class="col-sm-1 col-lg-2">
           <img
             src="../../../assets/img/icon_duplicate.png"
-            alt="nationality-icon"
+            alt="duplicate-icon"
             class="me-2"
           />
         </div>
         <div class="col-sm-9 col-lg-10">
           <h5>Stwórz nowe ogłoszenie na podstawie obecnego</h5>
           <p>(zaoszczędzisz czas dodajac pokoje w tym samym budynku)</p>
+        </div>
+      </div>
+    </div>
+    <div v-if="isActive" class="col-lg-6 col-xl-3">
+      <div class="d-flex pt-3">
+        <div class="col-sm-1 col-lg-2">
+          <img
+            src="../../../assets/img/icon_close.png"
+            alt="close-icon"
+            class="me-2"
+          />
+        </div>
+        <div class="col-sm-9 col-lg-10">
+          <h5>Zakończ ogłoszenie</h5>
+          <p>(ogłoszenie przejdzie do sekcji Nieaktywne)</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-lg-6 col-xl-3" v-else>
+      <div class="d-flex">
+        <div class="col-sm-1 col-lg-2">
+          <img
+            src="../../../assets/img/icon_post_advertisement.png"
+            alt="nationality-icon"
+            class="me-2"
+          />
+        </div>
+        <div class="col-sm-9 col-lg-10">
+          <h5>Opublikuj ogłoszenie</h5>
+          <p>
+            (nadal będziesz miał możliwość wprowadzania zmian w dowolnym
+            momencie)
+          </p>
+        </div>
+      </div>
+      <div class="d-flex pt-3">
+        <div class="col-sm-1 col-lg-2">
+          <img
+            src="../../../assets/img/icon_remove_advertisement.png"
+            alt="duplicate-icon"
+            class="me-2"
+          />
+        </div>
+        <div class="col-sm-9 col-lg-10">
+          <h5>Usuń ogłoszenie</h5>
+          <p>(ogłoszenie zostanie usunięte)</p>
         </div>
       </div>
     </div>
@@ -67,6 +114,9 @@ import Locator from "../details/Locator.vue";
 
 @Options({
   components: { Equipments, Host, Locator },
+  props: {
+    isActive: Boolean,
+  },
 })
 export default class SingleAd extends Vue {}
 </script>
--- src/views/HostAdvertisementsView.vue
+++ src/views/HostAdvertisementsView.vue
@@ -1,15 +1,41 @@
 <template>
   <div class="container-fluid host-advertisements">
-    <div class="row"><h3 class="px-4 pb-3">Moje ogłoszenia</h3></div>
-    <single-host-ad />
-    <single-host-ad />
-    <div class="d-flex justify-content-end align-items-center py-3">
-      <img
-        src="../assets/img/icon_duplicate.png"
-        alt="add advertisement"
-        class="me-3"
-      />
-      <h5>Dodaj nowe ogłoszenie</h5>
+    <div class="row">
+      <div class="col-sm-9"><h3 class="pb-3">Moje ogłoszenia</h3></div>
+      <div class="col-sm-3">
+        <button class="panel-expert-btn">Panel EXPERT</button>
+      </div>
+    </div>
+    <div class="active-ad pt-5">
+      <div class="d-flex active cursor-pointer pb-4">
+        <img
+          src="../assets/img/dot_green_big.png"
+          alt="green-dot"
+          class="me-2"
+        />
+        <h4>Aktywne</h4>
+      </div>
+      <single-host-ad :isActive="isActive" />
+      <single-host-ad :isActive="isActive" />
+      <div class="d-flex justify-content-end align-items-center py-3">
+        <img
+          src="../assets/img/icon_duplicate.png"
+          alt="add advertisement"
+          class="me-3"
+        />
+        <h5>Dodaj nowe ogłoszenie</h5>
+      </div>
+    </div>
+    <div class="inactive-ad pt-5">
+      <div class="d-flex inactive cursor-pointer pb-4">
+        <img
+          src="../assets/img/dot_orange_big.png"
+          alt="green-dot"
+          class="me-2"
+        />
+        <h4>Nieaktywne</h4>
+      </div>
+      <single-host-ad />
     </div>
   </div>
 </template>
@@ -21,7 +47,9 @@ import SingleHostAd from "../features/ad/list/SingleHostAd.vue";
 @Options({
   components: { SingleHostAd },
 })
-export default class HostAdvertisementsView extends Vue {}
+export default class HostAdvertisementsView extends Vue {
+  isActive = true;
+}
 </script>
 
 <style lang="scss">
@@ -31,8 +59,19 @@ export default class HostAdvertisementsView extends Vue {}
   span {
     font-weight: 600;
   }
-  h3 {
+
+  .active {
+    color: $success-color;
     border-bottom: 1px solid $dark-gray;
+  }
+  .inactive {
+    color: $primary-color;
+    border-bottom: 1px solid $dark-gray;
+  }
+  .panel-expert-btn {
+    height: 40px;
+    width: 200px;
+    border-radius: 5px;
   }
 }
 </style>
