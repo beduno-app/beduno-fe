d91ce04bd380ea1c62e324474a98a73e2c229740
superdyzio <perdekdawid@gmail.com>
2026-04-14 14:13:58 +0200

    [sonnet-4.6] add DE, UA, RU translation files
    All 5 target languages now registered in vue-i18n:
    PL, EN, DE, UA, RU. Core strings covered: common actions,
    auth, navigation, stay status labels.

--- src/app/plugins/i18n.ts
+++ src/app/plugins/i18n.ts
@@ -1,12 +1,15 @@
 import { createI18n } from 'vue-i18n'
 import pl from '@/assets/translations/pl'
 import en from '@/assets/translations/en'
+import de from '@/assets/translations/de'
+import ua from '@/assets/translations/ua'
+import ru from '@/assets/translations/ru'
 
 const i18n = createI18n({
   legacy: false,
   locale: 'pl',
   fallbackLocale: 'en',
-  messages: { pl, en },
+  messages: { pl, en, de, ua, ru },
 })
 
 export default i18n
